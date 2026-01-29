f96be342bef96182da0579576b7afc677edcf8de
Gouderg <victor.illien974@gmail.com>
2026-01-29 15:15:07 +0400

    Resume FR version

---
+++ src/js/header.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const about_section = document.getElementById("about");
+const resume_section = document.getElementById("resume");
+const folio_section = document.getElementById("portfolio");
+
+const about_header = document.getElementById("about_header");
+const resume_header = document.getElementById("resume_header");
+const folio_header = document.getElementById("portfolio_header");
+
+resume_header.style.setProperty("color", "var(--orange-100)");
+about_section.style.setProperty("display", "none");
+folio_section.style.setProperty("display", "none");
+
+about_header.addEventListener('click', () => {
+    about_header.style.setProperty("color", "var(--orange-100)");
+    resume_header.style.setProperty("color", "var(--white-600)");
+    folio_header.style.setProperty("color", "var(--white-600)");
+
+    resume_section.style.setProperty("display", "none");
+    folio_section.style.setProperty("display", "none");
+    about_section.style.setProperty("display", "flex");
+});
+
+resume_header.addEventListener('click', () => {
+    about_header.style.setProperty("color", "var(--white-600)");
+    resume_header.style.setProperty("color", "var(--orange-100)");
+    folio_header.style.setProperty("color", "var(--white-600)");
+
+    resume_section.style.setProperty("display", "flex");
+    folio_section.style.setProperty("display", "none");
+    about_section.style.setProperty("display", "none");
+});
+
+folio_header.addEventListener('click', () => {
+    about_header.style.setProperty("color", "var(--white-600)");
+    resume_header.style.setProperty("color", "var(--white-600)");
+    folio_header.style.setProperty("color", "var(--orange-100)");
+
+    resume_section.style.setProperty("display", "none");
+    folio_section.style.setProperty("display", "flex");
+    about_section.style.setProperty("display", "none");
+});
