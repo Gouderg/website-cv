70188270301930a64df1214922f49d9bcb62dd90
Gouderg <victor.illien974@gmail.com>
2026-01-30 15:45:05 +0400

    Portfolio

--- src/js/header.js
+++ src/js/header.js
@@ -8,9 +8,9 @@ const about_header = document.getElementById("about_header");
 const resume_header = document.getElementById("resume_header");
 const folio_header = document.getElementById("portfolio_header");
 
-about_header.style.setProperty("color", "var(--orange-100)");
+folio_header.style.setProperty("color", "var(--orange-100)");
 resume_section.style.setProperty("display", "none");
-folio_section.style.setProperty("display", "none");
+about_section.style.setProperty("display", "none");
 
 about_header.addEventListener('click', () => {
     about_header.style.setProperty("color", "var(--orange-100)");
