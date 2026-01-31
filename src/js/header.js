5c3a90159550b29a32459a30ed902eea1cc05f0f
Gouderg <victor.illien974@gmail.com>
2026-01-31 16:14:25 +0400

    Add icon to sidebar to toggle contacts

--- src/js/header.js
+++ src/js/header.js
@@ -41,3 +41,31 @@ folio_header.addEventListener('click', () => {
     folio_section.style.setProperty("display", "flex");
     about_section.style.setProperty("display", "none");
 });
+
+
+
+const side_show = document.getElementById("side-arrow");
+const side_contact = document.getElementById("side-contacts");
+const side_separation = document.getElementById("side-separation");
+const side_icons = document.getElementById("side-container-icons");
+const side_container = document.getElementById("side")
+
+side_show.addEventListener('click', () => {
+    side_contact.classList.toggle('side-show');
+    side_separation.classList.toggle('side-show');
+    side_icons.classList.toggle('side-show');
+
+    if (window.outerWidth <= 580) {
+        if (side_contact.classList.contains('side-show')) {
+            side_container.classList.add("side-max-width-min-content");
+            side_container.classList.remove("side-min-width-max-content");
+        } else {
+            side_container.classList.remove("side-max-width-min-content");
+            side_container.classList.add("side-min-width-max-content");
+        }
+    } else {
+        side_container.classList.remove("side-max-width-min-content");
+        side_container.classList.remove("side-min-width-max-content");
+    }
+    console.log()
+})
